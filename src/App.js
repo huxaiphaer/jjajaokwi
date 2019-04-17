60add4edbe5cd7a5e84ffa3dbd83fdda7c3557b1
huxaiphaer <l654321123>
2019-04-17 18:17:44 +0300

    - cause a conflict

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
 
+          <button>Click me </button>
           <a
             className="App-link"
             href="https://reactjs.org"
